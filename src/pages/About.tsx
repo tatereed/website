250bb96fd7176d4d03d56315795a0603978635d0
tatereed <76761406+tatereed@users.noreply.github.com>
2022-01-03 15:11:10 -0500

    About page made

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,7 +5,14 @@ import '../styles/About.css'
 function About(){
     return(
         <div className="About">
-            about
+            <div className="About-Contents">
+                <h1 className="About-Header">
+                    About
+                </h1>
+                <p className="Description">
+                    Tate is a 16 year old who enjoys programming things that people will never see. Tate has messed with several programming languages, but he has mostly settled on focusing on JavaScript/TypeScript.<br />This website was created to act as a lerning tool for Tate to learn React and TypeScript. The source code can be found on his GitHub.
+                </p>
+            </div>
         </div>
     );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,16 +12,16 @@ function Home(){
                     Welcome
                 </h1>
                 <p className="Welcome-Contents">
-                    This is my website that I created to act as a small portfolio. I have a link to my github as well as a blog section. Other than that, I do not have many current plans for the site. 
+                    This is Tate's website that he created to act as a small portfolio. There is a link to hisI github as well as a blog section. Other than that, there are not many current plans for the site. 
                 </p>
                 <h1 className="Find-Me-Header">
-                    Find Me
+                    Find Tate
                 </h1>
                 <p className="Find-Me-Contents">
-                    <a className="Github-Link" href="https://github.com/tatereed">
+                    <a className="Github-Link" href="https://github.com/tatereed" target="_blank" rel="noreferrer">
                         <Github size={32} />
                     </a>
-                    <a className="Reddit-Link" href="https://www.reddit.com/user/ttaereed">
+                    <a className="Reddit-Link" href="https://www.reddit.com/user/ttaereed" target="_blank" rel="noreferrer">
                         <Reddit size={32} />
                     </a>        
                 </p>
