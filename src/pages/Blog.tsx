0ac11469b44d247f1e1206a5305f17a3ded9bc93
tatereed <76761406+tatereed@users.noreply.github.com>
2022-01-03 15:35:24 -0500

    added contents to Blog page

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -5,7 +5,14 @@ import '../styles/Blog.css';
 function Blog(){
     return(
         <div className="Blog">
-            blog
+            <div className="Blog-Contents">
+                <h1 className="Blog-Header">
+                    Blog
+                </h1>
+                <p className="Blog-Description">
+                    Here is the blog page. I will put posts here when I make them.
+                </p>
+            </div>
         </div>
     );
 }
