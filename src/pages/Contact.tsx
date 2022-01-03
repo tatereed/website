2418d4b09f9871afb685759204634c9ff0687205
tatereed <76761406+tatereed@users.noreply.github.com>
2022-01-03 15:31:57 -0500

    contact page made

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,7 +5,14 @@ import '../styles/Contact.css'
 function Contact(){
     return(
         <div className="Contact">
-            contact
+            <div className="Contact-Contents">
+                <h1 className="Contact-Header">
+                    Contact
+                </h1>
+                <p>
+                    You can find me through my <a className="Github-Link-Contact" href="https://github.com/tatereed">GitHub</a> or <a className="Reddit-Link-Contact" href="https://www.reddit.com/user/ttaereed">Reddit</a>. If you want to contact me more directly, you can email me at <a className="Email-Link-Contact" href="mailto:contact.tatereed@gmail.com">contact.tatereed@gmail.com</a>
+                </p>
+            </div>
         </div>
     );
 }
