60d341a8036c066e9f668799f06fd3810af5b72f
tatereed <76761406+tatereed@users.noreply.github.com>
2022-11-09 13:37:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# website
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sveltejs-kit-template-default-btxrkp)
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	
+</script>
+
+<svelte:head>
+	<title>tatereeddotnet</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div>
+tatereeddotnet
+</div>
+
+<style>
+	
+</style>
